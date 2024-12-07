4abbbf2fe4e8be8f163ff8fbc267a0b6280b5d04
odevpedro <pedrosschmidt2@gmail.com>
2024-12-06 23:56:54 -0300

    finalizando api

--- src/BuscaApi.java
+++ src/BuscaApi.java
@@ -11,6 +11,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -22,8 +23,11 @@ public class BuscaApi {
         String busca = "";
         List<Titulo> titulos = new ArrayList<>();
 
-        while(!busca.equalsIgnoreCase("sair")){
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
 
+        while(!busca.equalsIgnoreCase("sair")){
 
         System.out.println("digite o nome do filme: ");
          busca = filme.nextLine();
@@ -33,17 +37,14 @@ public class BuscaApi {
          }
 
         String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=e8f4e026";
-            System.out.println(endereco);
+         System.out.println(endereco);
          HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().
+         HttpRequest request = HttpRequest.newBuilder().
                 uri(URI.create(endereco)).build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         String json = response.body();
-
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
 
         TituloOMDB meuTituloOmdb = gson.fromJson(json, TituloOMDB.class);
         System.out.println(meuTituloOmdb);
@@ -61,6 +62,11 @@ public class BuscaApi {
         } catch (ErroDeConversaoAnoExcpetion e){
             System.out.println(e.getMessage());
         }
+
+        FileWriter escrita = new FileWriter("filmes.json");
+        escrita.write(gson.toJson(titulos));
+        escrita.close();
+
         System.out.println("O programa finalizou corretamente!!!");
 
 
